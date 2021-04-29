144e2ec020bcd32eea4489db27748762ef675130
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-04-29 19:23:46 -0300

    criação do multiplexador do banco de regs e da ula

---
+++ processador/componentes/multiplexadores/multiplexador_ula.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity multiplexador_banco_ula is 
+    port(
+        ula_src : in std_logic;
+        dado_lido  
+    );
+end multiplexador_banco_ula;
+
+architecture multiplexador_comportamento of multiplexador_banco_ula
+    begin
+        
+    end multiplexador_comportamento;
