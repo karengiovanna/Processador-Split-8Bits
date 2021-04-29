bbc388c1b8cfa966a84799645e5ea0e1cfb6d816
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-04-29 19:22:59 -0300

    Criação do add

---
+++ processador/componentes/and/and.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity and_componente is 
+    port(
+        entrada_and_1   : in std_logic;
+        entrada_and_2   : in std_logic;
+
+        saida_and       : out std_logic
+    );
+end and_componente;
+
+architecture and_comportamento of and_componente
+    begin
+        saida_and <= entrada_and_1 and entrada_and_2;
+    end and_componente;
