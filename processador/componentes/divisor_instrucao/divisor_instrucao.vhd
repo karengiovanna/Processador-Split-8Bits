a75f0ae0bcfbf45de9497cd3653d00ffe60a99fb
João Paulo <joaoprrn@hotmail.com>
2021-05-03 15:40:04 -0400

    corrigimos os índices dos vetores

--- processador/componentes/divisor_instrucao/divisor_instrucao.vhd
+++ processador/componentes/divisor_instrucao/divisor_instrucao.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity divisor_instrucao is
     port(
-        saida_extensor1 :   out std_logic_vector(4 downto 0);
+        saida_extensor1 :   out std_logic_vector(3 downto 0);
         
         entrada_divisor :   in std_logic_vector (7 downto 0);
         saida_uc        :   out std_logic_vector(3 downto 0);
@@ -16,7 +16,7 @@ end divisor_instrucao;
 
 architecture comportamento_divisor of divisor_instrucao is
     begin
-        saida_extensor1 <= entrada_divisor(4 downto 0);
+        saida_extensor1 <= entrada_divisor(3 downto 0);
 
         saida_uc        <= entrada_divisor(7 downto 4);
         saida_reg1      <= entrada_divisor(3 downto 2);
