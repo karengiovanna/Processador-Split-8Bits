37f1240e0e1f02e01baec4ec53da15e06e816177
João Paulo <joaoprrn@hotmail.com>
2021-05-03 14:55:20 -0400

    Corrigimos os erros de sintaxe do componente

--- processador/componentes/banco_registradores/banco_registradores.vhd
+++ processador/componentes/banco_registradores/banco_registradores.vhd
@@ -1,7 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+USE ieee.std_logic_arith.ALL;
 
-entity banco_regs is
+entity banco_registradores is
     port(
         entrada_reg1            : in std_logic_vector(3 downto 2);
         entrada_reg2            : in std_logic_vector(1 downto 0);
@@ -12,17 +14,21 @@ entity banco_regs is
 
         reg_write : in std_logic
     );
-end banco_regs;
+end banco_registradores;
 
-architecture comportamento_banco_regs of banco_regs is
-    type registradores is array (0 to 3) of std_logic_vector (7 downto 0);
+architecture comportamento_banco_registradores of banco_registradores is
+
+    type banco_registrador is array (0 to 3) of std_logic_vector (7 downto 0);
+    signal registrador : banco_registrador;
+
     begin
         process (entrada_reg1)
+			begin
             if (reg_write = '1') then -- se reg_write == 1 escreve o dado no banco de registradores
-                registradores(to_integer(unsigned(entrada_reg1))) <= entrada_escrita_dados;  -- entrar no indice do vetor para salvar o dado 
+                registrador(conv_integer(unsigned(entrada_reg1))) <= entrada_escrita_dados;  -- entrar no indice do vetor para salvar o dado 
             end if;
-            saida_dado_lido1 <= registradores(to_integer(unsigned(entrada_reg1))); 
-            saida_dado_lido2 <= registradores(to_integer(unsigned(entrada_reg2))) 
+            saida_dado_lido1 <= registrador(conv_integer(unsigned(entrada_reg1))); 
+            saida_dado_lido2 <= registrador(conv_integer(unsigned(entrada_reg2)));
         end process;
 
-end comportamento_banco_regs;
+end comportamento_banco_registradores;
