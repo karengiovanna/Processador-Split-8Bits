ffc421de230e324afb673444a79aaae6bcd614a8
João Paulo <joaoprrn@hotmail.com>
2021-05-04 19:50:41 -0400

    Corrigimos a sintaxe e fizemos waveform

--- processador/componentes/multiplexadores/multiplexador_Branch_and.vhd
+++ processador/componentes/multiplexadores/multiplexador_Branch_and.vhd
@@ -6,16 +6,19 @@ entity multiplexador_Branch_and is
         resultado_and           : in std_logic;
         resultado_extensor_2_8  : in std_logic_vector(7 downto 0);
         resultado_add           : in std_logic_vector(7 downto 0);
-        saida_mux_branch_and    : out std_logic_vector(7 downto 0);
+        saida_mux_branch_and    : out std_logic_vector(7 downto 0)
     );
 end multiplexador_Branch_and;
 
-architecture mux_comportamento_and of multiplexador_Branch_and
+architecture mux_comportamento_and of multiplexador_Branch_and is
     begin
-        case(resultado_and) is
-            when '1' =>
-                saida_mux_branch_and <= resultado_extensor_2_8;
-            when '0' =>
-                saida_mux_branch_and <= resultado_add;
-        end case;
+		  process(resultado_and, resultado_extensor_2_8, resultado_add)
+		  begin
+			  case(resultado_and) is
+					when '1' =>
+						 saida_mux_branch_and <= resultado_extensor_2_8;
+					when '0' =>
+						 saida_mux_branch_and <= resultado_add;
+			  end case;
+		  end process;
     end mux_comportamento_and;
