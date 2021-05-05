0fcb3378b0d964faa7fc1d6f0ca507922ff7132c
João Paulo <joaoprrn@hotmail.com>
2021-05-04 20:02:31 -0400

    Correção do multiplexador_jump e waveform

--- processador/componentes/multiplexadores/multiplexador_jump.vhd
+++ processador/componentes/multiplexadores/multiplexador_jump.vhd
@@ -6,16 +6,19 @@ entity multiplexador_jump is
         flag_jump                       : in std_logic;
         resultado_extensor_2_8          : in std_logic_vector(7 downto 0);
         resultado_mux_branch_and        : in std_logic_vector(7 downto 0);
-        saida_mux_jump                  : out std_logic_vector(7 downto 0);
+        saida_mux_jump                  : out std_logic_vector(7 downto 0)
     );
 end multiplexador_jump;
 
-architecture mux_comportamento_jump of multiplexador_jump
+architecture mux_comportamento_jump of multiplexador_jump is
     begin
-        case(flag_jump) is
-            when '1' =>
-                saida_mux_jump <= resultado_extensor_2_8;
-            when '0' =>
-                saida_mux_jump <= resultado_mux_branch_and;
-        end case;
+		  process(flag_jump, resultado_extensor_2_8, resultado_mux_branch_and)
+				begin
+			  case(flag_jump) is
+					when '1' =>
+						 saida_mux_jump <= resultado_extensor_2_8;
+					when '0' =>
+						 saida_mux_jump <= resultado_mux_branch_and;
+			  end case;
+		end process;
     end mux_comportamento_jump;
