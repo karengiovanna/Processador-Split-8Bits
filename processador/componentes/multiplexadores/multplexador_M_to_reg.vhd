d93422bb18c2054a78ff3d4297692ee252b33d3b
João Paulo <joaoprrn@hotmail.com>
2021-05-04 20:15:15 -0400

    Correção multiplexador_M_to_reg e waveform

--- processador/componentes/multiplexadores/multplexador_M_to_reg.vhd
+++ processador/componentes/multiplexadores/multplexador_M_to_reg.vhd
@@ -6,16 +6,19 @@ entity multiplexador_M_to_reg is
         M_to_reg                : in std_logic;
         dado_lido2              : in std_logic_vector(7 downto 0);
         resultado_ula           : in std_logic_vector(7 downto 0);
-        saida_mux_M_to_reg      : out std_logic_vector(7 downto 0);
+        saida_mux_M_to_reg      : out std_logic_vector(7 downto 0)
     );
 end multiplexador_M_to_reg;
 
-architecture mux_comportamento_M_to_reg of multiplexador_M_to_reg
+architecture mux_comportamento_M_to_reg of multiplexador_M_to_reg is
     begin
-        case(M_to_reg) is
-            when '1' =>
-                saida_mux_M_to_reg <= dado_lido2;
-            when '0' =>
-                saida_mux_M_to_reg <= resultado_ula;
-        end case;
+        process(M_to_reg, dado_lido2, resultado_ula)
+            begin
+            case(M_to_reg) is
+                when '1' =>
+                    saida_mux_M_to_reg <= dado_lido2;
+                when '0' =>
+                    saida_mux_M_to_reg <= resultado_ula;
+            end case;
+        end process;
     end mux_comportamento_M_to_reg;
