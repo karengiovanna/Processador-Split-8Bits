989e5205ee129ff06c989ccbd78429573fd3430f
João Paulo <joaoprrn@hotmail.com>
2021-05-04 20:09:21 -0400

    Correção do multiplexador_ula_src e waveform

--- processador/componentes/multiplexadores/multiplexador_ula_src.vhd
+++ processador/componentes/multiplexadores/multiplexador_ula_src.vhd
@@ -6,16 +6,19 @@ entity multiplexador_ula_src is
         ula_src                 : in std_logic;
         dado_lido2              : in std_logic_vector(7 downto 0);
         saida_extensor          : in std_logic_vector(7 downto 0);
-        saida_mux_ula_src       : out std_logic_vector(7 downto 0);
+        saida_mux_ula_src       : out std_logic_vector(7 downto 0)
     );
 end multiplexador_ula_src;
 
-architecture mux_comportamento_ula of multiplexador_ula_src
+architecture mux_comportamento_ula of multiplexador_ula_src is
     begin
-        case(ula_src) is
-            when '0' =>
-                saida_mux_ula_src <= dado_lido2;
-            when '1' =>
-                saida_mux_ula_src <= saida_extensor;
-        end case;
+        process(ula_src, dado_lido2, saida_extensor)
+        begin
+            case(ula_src) is
+                when '0' =>
+                    saida_mux_ula_src <= dado_lido2;
+                when '1' =>
+                    saida_mux_ula_src <= saida_extensor;
+            end case;
+        end process;
     end mux_comportamento_ula;
